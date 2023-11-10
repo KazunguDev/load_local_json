ea1833c5d23ce64d48d37a788b77775d35c9049d
kazungudev <kazungudev@gmail.com>
2023-11-10 15:56:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# load_local_json
+
+Example app showing implementation which loads up a json file from a local folder.
+
+## 
---
+++ android/app/src/main/kotlin/com/kazungudev/load_local_json/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kazungudev.load_local_json
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
